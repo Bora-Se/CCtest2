adcda0ecf6a8a4c56261e169c92ba6a96cb11e6a
Bora-Se <114330033+Bora-Se@users.noreply.github.com>
2022-12-03 22:55:49 +0100

    3.12

--- src/test/java/runners/TestRunner1.java
+++ src/test/java/runners/TestRunner1.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue="stepdefinitions",
-        tags= "@US31",
+        tags= "@demy",
         dryRun = false
 )
 
