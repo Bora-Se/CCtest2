b11807bd32cb4c6f93962a5a3c58b0afe73bd220
Bora-Se <114330033+Bora-Se@users.noreply.github.com>
2022-12-26 22:20:44 +0100

    26.12

--- src/test/java/runners/TestRunner1.java
+++ src/test/java/runners/TestRunner1.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue="stepdefinitions",
-        tags= "@US03",
+        tags= "@levent",
         dryRun = false
 )
 
