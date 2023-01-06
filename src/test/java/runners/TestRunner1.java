e834f149bd698ea70a2a22df54bec918f20dbeaa
Bora-Se <114330033+Bora-Se@users.noreply.github.com>
2023-01-06 19:43:21 +0100

    6.jan

--- src/test/java/runners/TestRunner1.java
+++ src/test/java/runners/TestRunner1.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue="stepdefinitions",
-        tags= "@US20",
+        tags= "@US18",
         dryRun = false
 )
 
